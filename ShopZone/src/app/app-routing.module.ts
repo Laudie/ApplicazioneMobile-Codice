7c1aa0332b24d3523cc8cc3f605e0cfe4aa62cda
StefanoTassoni <stefano.tassoni91@gmail.com>
2019-06-05 14:13:12 +0200

    codice base

--- ShopZone/src/app/app-routing.module.ts
+++ ShopZone/src/app/app-routing.module.ts
@@ -2,25 +2,12 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'tabs/notizie',
-        pathMatch: 'full'
-    },
-    {
-        path: 'notizie',
-        loadChildren: './pages/login/login.module#NotiziePageModule'
-    },
-    {
-        path: 'login',
-        loadChildren: './pages/login/login.module#LoginPageModule'
-    },
-    {
-        path: 'tabs',
-        loadChildren: './pages/tabs/tabs.module#TabsPageModule'
-    },
-    {path: 'impostazioni', loadChildren: './pages/impostazioni/impostazioni.module#ImpostazioniPageModule'},
-    {path: 'preferiti', loadChildren: './pages/preferiti/preferiti.module#PreferitiPageModule'}
+    {path: '', loadChildren: 'tabs/notizie', pathMatch: 'full'},
+    {path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},
+    {path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule'},
+    {path: 'notizie', loadChildren: './pages/tabs/tabs.module#NotiziePageModule'},
+    {path: 'impostazioni', loadChildren: './pages/tabs/tabs.module#ImpostazioniPageModule'},
+    {path: 'preferiti', loadChildren: './pages/tabs/tabs.module#PreferitiPageModule'}
 ];
 
 @NgModule({
