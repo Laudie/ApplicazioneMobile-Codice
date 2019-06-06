e59135fd44492cecfa25be314d766be362109df1
Laudie <laura2804lde@gmail.com>
2019-06-06 15:39:11 +0200

    prova

--- ShopZone/src/app/app-routing.module.ts
+++ ShopZone/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: '', loadChildren: './pages/tabs/notizie', pathMatch: 'full'},
+    {path: '', redirectTo: 'tabs', pathMatch: 'full'},
     {path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},
     {path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule'}
 ];
