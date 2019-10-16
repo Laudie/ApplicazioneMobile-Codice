ebe8a45863d48dbd7f7588085b4f77521f62138d
Laudie <laura2804lde@gmail.com>
2019-10-16 19:06:46 +0200

    registrazione

--- ShopZone/src/app/pages/registrazione/registrazione.page.ts
+++ ShopZone/src/app/pages/registrazione/registrazione.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ModalController, NavParams} from '@ionic/angular';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-registrazione',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrazionePage implements OnInit {
 
-  constructor() { }
+  private registrazioneFormModel: FormGroup;
+  constructor(
+      private formBuilder: FormBuilder,
+      private modalController: ModalController
+  ) { }
 
   ngOnInit() {
+    this.registrazioneFormModel = this.formBuilder.group({
+      name: ['Laura', Validators.compose([Validators.required
+      ])],
+      surname: ['Di Egidio', Validators.compose([
+        Validators.required
+      ])],
+      username: ['Laurett', Validators.compose([
+        Validators.required
+      ])],
+      password: ['Laura10', Validators.compose([
+        Validators.required
+      ])]
+    });
   }
-
-}
+  async onSubmit() {
+  }
+  }
