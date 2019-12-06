d9fb0a6020e7382dec795f59406fa731d712e9ef
StefanoTassoni <stefano.tassoni91@gmail.com>
2019-12-06 16:35:05 +0100

    modifica tab bar  negoziante personalizzata

--- ShopZone/src/app/pages/impostazioni/impostazioni.page.ts
+++ ShopZone/src/app/pages/impostazioni/impostazioni.page.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ModalController, NavController} from '@ionic/angular';
-import {AggiungiNegozioPage} from '../aggiungi-negozio/aggiungi-negozio.page';
+// import {AggiungiNegozioPage} from '../aggiungi-negozio/aggiungi-negozio.page';
 import {UtenteService} from '../../services/utente.service';
 import {BehaviorSubject} from 'rxjs';
 import {Utente} from '../../model/utente.model';
+import {Lingua, LinguaService} from '../../services/lingua.service';
 
 @Component({
   selector: 'app-impostazioni',
@@ -12,22 +13,28 @@ import {Utente} from '../../model/utente.model';
 })
 export class ImpostazioniPage implements OnInit {
   private utente$: BehaviorSubject<Utente>;
-
+  private lingue: Lingua[];
   constructor(private modalController: ModalController,
-              private utenteService: UtenteService) { }
+              private utenteService: UtenteService,
+              private linguaService: LinguaService) { }
 
   ngOnInit() {
       if (this.utenteService.isLogged()) {
         this.utente$ = this.utenteService.getUtente();
       }
+      //Campo lingue
+      //this.lingua = this.linguaServices.getLingue();
+
   }
+  /*
   async aggiungiNegozio() {
+
     const myModal = await this.modalController.create({
       component: AggiungiNegozioPage
     });
     await myModal.present();
   }
-
+*/
   logout() {
     this.utenteService.logout();
   }
